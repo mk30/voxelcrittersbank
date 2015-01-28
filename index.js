e9bd06f36c711f99544058eeacbec1a74c3a01d9
Marina Kukso <marina.kukso@gmail.com>
2015-01-28 15:42:09 -0800

    trying out forked voxel-painter-admin

---
+++ index.js
@@ -0,0 +1,59 @@
+var Handlebars = require('handlebars');
+var request    = require('browser-request');
+
+var content = document.getElementById('critters-list-template').textContent;
+var renderCrittersList = Handlebars.compile(content);
+
+request.couch({
+    method: 'GET',
+    url: 'http://cors.maxogden.com/http://max.ic.ht/critters/_all_docs?include_docs=true'
+}, function (err, res, body) {
+    var critters = []
+    body.rows.map(function(row) {
+      if (row.id.match(/^_design/)) return
+      critters.push(row)
+    })
+    renderCritters(createCritter(critters));
+});
+
+function deleteCritters(docs) {
+  docs = docs.map(function(d) { d._deleted = true; return d; })
+  console.log(
+    "curl -X POST http://max.ic.ht/critters/_bulk_docs -d '" +
+    JSON.stringify({docs: docs}) +
+    "' -H 'content-type: application/json'"
+  )
+}
+
+function renderCritters (critters) {
+    var wrapper = document.getElementById('critters-wrapper');
+    
+    wrapper.innerHTML = renderCrittersList({'critters': critters});
+    document.querySelector('body').addEventListener('click', function (e) {
+        if (e.target.className === 'btn-delete') {
+            var checkboxNodes = document.querySelectorAll('input[type="checkbox"]')
+            var checked = []
+            Array.prototype.slice.call(checkboxNodes).map(function(checkbox) {
+                if (!checkbox.checked) return
+                checked.push({
+                  _id: checkbox.attributes['data-id'].textContent,
+                  _rev: checkbox.attributes['data-rev'].textContent
+                })
+            })
+            if (checked.length === 0) return
+            deleteCritters(checked)
+        }
+    });
+}
+
+function createCritter (data) {
+    return data.map(function (row) {
+        return {
+            id: row.id,
+            rev: row.doc._rev,
+            author: row.doc.author,
+            link: row.doc.link,
+            name: row.doc.name
+        };
+    });
+}
